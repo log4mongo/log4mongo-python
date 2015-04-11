d9cb089503cfa80124da4c68ec09a19b80da93f6
Oz Nahum Tiram <nahumoz@gmail.com>
2015-04-11 19:44:39 +0200

    pep8

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ import os
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='log4mongo',
